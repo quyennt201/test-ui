f18cdd93588861167b6f3796dafb737601b5677a
quyennt201 <quyennt.201@gmail.com>
2026-03-20 13:18:35 +0700

    fix: build

--- src/components/common/ButtonDropdown.tsx
+++ src/components/common/ButtonDropdown.tsx
@@ -36,6 +36,7 @@ export default function ButtonDropdown({
         return () => {
             document.removeEventListener("click", handleClickOutside);
         };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     useEffect(() => {
